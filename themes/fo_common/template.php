7b9469ea8fb25f7ce7f2bbdf4f32e74c02783e37
Richard B. Porter <richard-b-porter@uiowa.edu>
2017-04-10 10:43:53 -0500

    clean up fo_common template.php

--- themes/fo_common/template.php
+++ themes/fo_common/template.php
@@ -12,7 +12,7 @@
  * used as an alternative to directly editing or adding code to templates. Its
  * worth spending some time to learn more about these functions - they are a
  * powerful way to easily modify the output of any template variable.
- * 
+ *
  * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
  * 1. Rename each function and instance of "fo_common" to match
  *    your subthemes name, e.g. if your theme name is "footheme" then the function
@@ -21,6 +21,9 @@
  * 2. Uncomment the required function to use.
  */
 
+/**
+ * Implements template_preprocess_field().
+ */
 function fo_common_preprocess_field(&$variables, $hook) {
   // Add sub-labels to Records fields with HTML classes for styling
   switch($variables['element']['#field_name']) {
@@ -65,75 +68,3 @@ function fo_common_block_view_book_navigation_alter(&$data, $block) {
     }
   }
 }
-
-
-/**
- * Preprocess variables for the html template.
- */
-/* -- Delete this line to enable.
-function fo_common_preprocess_html(&$vars) {
-  global $theme_key;
-
-  // Two examples of adding custom classes to the body.
-  
-  // Add a body class for the active theme name.
-  // $vars['classes_array'][] = drupal_html_class($theme_key);
-
-  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
-  // $vars['classes_array'][] = css_browser_selector();
-
-}
-// */
-
-
-/**
- * Process variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function fo_common_process_html(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables for the page templates.
- */
-/* -- Delete this line if you want to use these functions
-function fo_common_preprocess_page(&$vars) {
-}
-function fo_common_process_page(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the node templates.
- */
-/* -- Delete this line if you want to use these functions
-function fo_common_preprocess_node(&$vars) {
-}
-function fo_common_process_node(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function fo_common_preprocess_comment(&$vars) {
-}
-function fo_common_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function fo_common_preprocess_block(&$vars) {
-}
-function fo_common_process_block(&$vars) {
-}
-// */
