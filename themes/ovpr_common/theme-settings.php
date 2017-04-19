fcc899696d978951b311e2a757e62201e6a8b45e
Brandon Neil <brandon-neil@uiowa.edu>
2017-04-19 15:17:13 -0500

    Remove some commented out code in ovpr_common (#115)

--- themes/ovpr_common/theme-settings.php
+++ themes/ovpr_common/theme-settings.php
@@ -10,7 +10,7 @@
  * @param $form: Nested array of form elements that comprise the form.
  * @param $form_state: A keyed array containing the current state of the form.
  */
- 
+
 function ovpr_common_form_system_theme_settings_alter(&$form, &$form_state)  {
 
   $form['at-settings']['ovpr'] = array(
@@ -47,17 +47,3 @@ function ovpr_common_form_system_theme_settings_alter(&$form, &$form_state)  {
     ),
   );
 }
-/*<div id="research-link"><a href="http://research.uiowa.edu">UNIVERSITY OF IOWA &nbsp;</a></div>
-<div id="current-dept-link">
-  <a class="dept-link" href="<?php echo $GLOBALS['base_url']; ?>"><?php echo $ovpr_full_name; ?> <!--Researcher Handbook--></a>
-</div>
-    	<ul id="depts-links-top">
-        <li class="first"><a title="Conflict Of Interest" href="http://research.uiowa.edu/coi">COI</a></li>
-        <li><a title="Division of Sponsored Programs" href="http://research.uiowa.edu/dsp">DSP</a></li>
-        <li><a title="Environmental Health & Safety" href="http://research.uiowa.edu/ehs">EHS</a></li>
-        <li><a title="Human Subjects Office / IRB" href="http://research.uiowa.edu/hso">HSO/IRB</a></li>
-        <li><a title="Office of Animal Resources / Institutional Animal Care and USe Committee" href="http://animal.research.uiowa.edu">OAR/IACUC</a></li>
-        <li><a title="Research Information Systems" href="http://research.uiowa.edu/ris">RIS</a></li>
-    		<li><a title="Office of the Vice President for Research" href="http://research.uiowa.edu">VPR</a></li>  		
-    		<li class="last"><a title="See More Units..." href="https://research.uiowa.edu/ovpr/ovpr-research-units-facilities-and-centers-directory">MORE UNITS</a></li>
-    	</ul>*/
