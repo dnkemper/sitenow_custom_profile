5d22db8ee04d7e9aeb4ebf7127c48dd1333f03c6
Genevieve Johnson <genevieve-johnson@uiowa.edu>
2017-04-21 15:08:20 -0500

    remove commented out code

--- themes/parallaxy/parallaxy_core/template.php
+++ themes/parallaxy/parallaxy_core/template.php
@@ -27,14 +27,6 @@
 function parallaxy_preprocess_html(&$vars) {
   global $theme_key;
 
-  // Two examples of adding custom classes to the body.
-
-  // Add a body class for the active theme name.
-  // $vars['classes_array'][] = drupal_html_class($theme_key);
-
-  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
-  // $vars['classes_array'][] = css_browser_selector();
-
   // Implements body class for theme-setting color palette otpions
   $vars['classes_array'][] = theme_get_setting('content_wrapper_pattern');
 
@@ -46,56 +38,3 @@ function parallaxy_preprocess_html(&$vars) {
     drupal_add_js(drupal_get_path('theme', 'parallaxy') . '/scripts/parallaxy.js');
   }
 }
-
-
-/**
- * Process variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function parallaxy_process_html(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables for the page templates.
- */
-/* -- Delete this line if you want to use these functions
-function parallaxy_preprocess_page(&$vars) {
-}
-function parallaxy_process_page(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the node templates.
- */
-/* -- Delete this line if you want to use these functions
-function parallaxy_preprocess_node(&$vars) {
-}
-function parallaxy_process_node(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function parallaxy_preprocess_comment(&$vars) {
-}
-function parallaxy_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function parallaxy_preprocess_block(&$vars) {
-}
-function parallaxy_process_block(&$vars) {
-}
-// */
