18a3253450af10317a38e422bea6571eeb4e0fe4
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2017-06-15 13:46:57 -0500

    add engineering theme.

---
+++ themes/engineering/templates/contrib/uiowa-events-pane--grid.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file uiowa-events-pane.tpl.php
+ * Default theme implementation for the events pane.
+ *
+ * @see uiowa_events_pane()
+ *
+ * Available variables:
+ *
+ * $data
+ *   Event data generated from Localist API.
+ */
+$view_all = l('View Full Calendar', base_path() . 'events');
+?>
+<div class="row">
+  <?php foreach ($items as $delta => $item): ?>
+    <div class="col col-md-4">
+      <?php print render($item); ?>
+    </div>
+  <?php endforeach; ?>
+</div>
+<div class="view-all"><?php print $view_all; ?></div>
---
+++ themes/engineering/theme-settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Theme settings.
+ */
+
+/**
+ * Implements theme_settings().
+ */
+function engineering_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Ensure this include file is loaded when the form is rebuilt from the cache.
+  $form_state['build_info']['files']['form'] = drupal_get_path('theme', 'engineering') . '/theme-settings.php';
+
+  // Add theme settings here.
+  $form['engineering_theme_settings'] = array(
+    '#title' => t('Theme Settings'),
+    '#type' => 'fieldset',
+  );
+
+  // Return the additional form widgets.
+  return $form;
+}
