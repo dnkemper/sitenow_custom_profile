14a82fd263a0492eb4b81e26455e43e33932929d
dhannad <dawn-hanna@uiowa.edu>
2018-06-12 09:33:16 -0500

    add events description preprocess to ovpr_common

--- themes/ovpr_common/templates/uiowa-events-teaser.tpl.php
+++ themes/ovpr_common/templates/uiowa-events-teaser.tpl.php
@@ -15,7 +15,16 @@
  */
 ?>
 <div class="uiowa-event">
-  <p class="date-string"><?php print render($variables['event']['date_string']); ?></p>
-  <?php print render($variables['event']['image']); ?>
-  <h3><a href="<?php print $variables['event']['url']; ?>"><?php print $variables['event']['title']; ?></a></h3>
+  <p class="date-string uiowa-event-date"><?php print render($variables['event']['date_string']); ?></p>
+  <h3 class="uiowa-event-title"><a href="<?php print $variables['event']['url']; ?>"><?php print $variables['event']['title']; ?></a></h3>
+    <?php if (isset($variables['event']['image'])): ?>
+        <div class="col-sm-2 uiowa-event-image"><?php print render($variables['event']['image']); ?></div>
+        <div class="col-sm-10 uiowa-event-description"><?php print render($variables['event']['description']); ?></div>
+    <?php else: ?>
+        <div class="uiowa-event-description"><?php print render($variables['event']['description']); ?></div>
+    <?php endif; ?>
+
+    <?php if (isset($variables['event']['location_name'])): ?>
+        <div class="uiowa-event-location">Location: <?php print render($variables['event']['location_name']); ?></div>
+    <?php endif; ?>
 </div>
