3e36d12c9d56398def810689d47cdb60f2b1feb6
Adam Delaney <adam-delaney@uiowa.edu>
2018-10-29 15:50:57 -0500

    updated js selector

--- modules/custom/sitenow_slick_a11y/js/sitenow_slick_a11y_dots.behavior.js
+++ modules/custom/sitenow_slick_a11y/js/sitenow_slick_a11y_dots.behavior.js
@@ -7,7 +7,7 @@
   Drupal.behaviors.slickA11yDots = {
     attach: function (context, settings) {
       jQuery.each( Drupal.settings.slick_a11y.slickA11y, function( i, settings ) {
-        $('#slick-' + settings.id ).once('slick-a11y-dots', function () {
+        $('.slick').once('slick-a11y-dots', function () {
           // Add arial-label and titles to dot li.
           var dots = $(this).find(".slick-dots li");
           $.each(dots,function( index, dot ) {
--- modules/custom/sitenow_slick_a11y/js/sitenow_slick_a11y_events.behavior.js
+++ modules/custom/sitenow_slick_a11y/js/sitenow_slick_a11y_events.behavior.js
@@ -7,7 +7,7 @@
   Drupal.behaviors.slickA11yEvents = {
     attach: function (context, settings) {
       jQuery.each( Drupal.settings.slick_a11y.slickA11y, function( i, settings ) {
-        $('#slick-' + settings.id ).once('slick-a11y-events', function () {
+        $('.slick').once('slick-a11y-events', function () {
           // When a slide is changed.
           $(this).on('init', function( slick ) {
             var currentSlide = 0;
