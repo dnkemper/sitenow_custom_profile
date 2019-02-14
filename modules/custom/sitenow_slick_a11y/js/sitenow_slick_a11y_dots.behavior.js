e95f21f8e6b901f09e1bef7100a15e16c8b0b672
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2019-02-14 15:09:49 -0600

    new branch

--- modules/custom/sitenow_slick_a11y/js/sitenow_slick_a11y_dots.behavior.js
+++ modules/custom/sitenow_slick_a11y/js/sitenow_slick_a11y_dots.behavior.js
@@ -9,7 +9,7 @@
       jQuery.each( Drupal.settings.slick_a11y.slickA11y, function( i, settings ) {
         $('#' + settings.id).once('slick-a11y-dots', function () {
           // Add arial-label and titles to dot li.
-          var dots = $(this).find(".slick-dots li");
+          var dots = $(this).find(".slick-dots li ");
           $.each(dots,function( index, dot ) {
             $(this).attr('title', 'Jump to ' + settings.titles[index]).attr('aria-label', 'Jump to ' + settings.titles[index]);
           });
